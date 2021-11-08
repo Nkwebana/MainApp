4f6c354990c89cbe791e9f7055463e12b244390b
Buyisile Nkwebana <buyisilenkwebana@Buyisiles-MacBook-Air.local>
2021-11-08 23:29:54 +0200

    wip

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
+import { View } from 'react-native';
 import { useStoreActions, useStoreState } from 'easy-peasy';
 import ModuleA from 'modulea-bn';
+import ModuleB from 'moduleb-bn';
 
 function Home() {
   const updateModuleState = useStoreActions(
@@ -12,15 +14,27 @@ function Home() {
     updateModuleState(data);
   };
 
+  const changeModuleAState = (data) => {
+    updateModuleState(data);
+  };
+
   useEffect(() => {
     console.log('moduleInfo: ', moduleInfo);
   }, [moduleInfo]);
 
   return (
-    <ModuleA
-      message="This is ModuleA Component"
-      handleStateUpdate={handleStateUpdate}
-    />
+    <View>
+      <ModuleA
+        message="This is ModuleA Component"
+        handleStateUpdate={handleStateUpdate}
+      />
+
+      <ModuleB
+        message="This is ModuleB Component"
+        backgroundColor={moduleInfo.modulebBackgroundColor}
+        changeModuleAState={changeModuleAState}
+      />
+    </View>
   );
 }
 
