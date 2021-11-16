99c969ee685db169b4635815b7aedb1193d64d67
Buyisile Nkwebana <buyisilenkwebana@Buyisiles-MacBook-Air.local>
2021-11-16 14:21:10 +0200

    added login screen

--- src/components/NavigationManager/index.js
+++ src/components/NavigationManager/index.js
@@ -4,15 +4,17 @@
  *
  */
 
-import * as React from 'react';
+import React, { useState, useEffect, createRef } from 'react';
 import PropTypes from 'prop-types';
 import { useStoreActions } from 'easy-peasy';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import ProductModule from 'productmodule-bn';
+import Login from 'modulea-bn';
 
 function NavigationManager({ products }) {
   const Stack = createNativeStackNavigator();
+  const containerRef = createRef();
 
   const addCartItem = useStoreActions(
     (actions) => actions.cart.add
@@ -22,13 +24,26 @@ function NavigationManager({ products }) {
     addCartItem(cartInfo)
   }
 
+  const navigate = (name, params) => {
+    containerRef.current && containerRef.current.navigate(name, params);
+  };
+
+  const handleLogin = (loginData) => {
+    navigate('Products')
+    console.log('loginData: ', loginData);
+  }
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Products">
+    <NavigationContainer ref={containerRef}>
+      <Stack.Navigator initialRouteName="Login Screen">
         <Stack.Screen name="Products" options={{
-          headerShown: false
+          headerShown: false,
         }}>
           {props => <ProductModule {...props} products={products} addToCart={addToCart} isParentNavigating={true} />}
+        </Stack.Screen>
+
+        <Stack.Screen name="Login Screen">
+          {props => <Login {...props} handleLogin={handleLogin} />}
         </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
