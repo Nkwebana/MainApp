f457529f98fb534d4dffac494d597b56f83a0a1c
Buyisile Nkwebana <buyisilenkwebana@Buyisiles-MacBook-Air.local>
2021-11-17 11:46:13 +0200

    wip

--- src/components/NavigationManager/index.js
+++ src/components/NavigationManager/index.js
@@ -12,7 +12,22 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import ProductModule from 'productmodule-bn';
 import Login from 'modulea-bn';
 
-function NavigationManager({ products }) {
+function NavigationManager({ products = [{
+  id: 1,
+  productName: 'Chicken',
+  productPrice: 75,
+  productImage: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-190808-baked-drumsticks-0205-portrait-pf-1567089281.jpg?crop=1.00xw:0.667xh;0,0.150xh&resize=768:*',
+  productSize: '500ml',
+  productDescription: 'This is coolest product ever.'
+}, {
+  id: 2,
+  productName: 'Coffee',
+  productPrice: 50,
+  productImage: 'https://joya.info/images/content/438914-1200x700c56x50-Kaffee-mit-pflanzlicher-Milch-Barista_1200x798.jpg',
+  productSize: '500ml',
+  productDescription: 'This is coolest product ever.'
+}] }) {
+
   const Stack = createNativeStackNavigator();
   const containerRef = createRef();
 
@@ -28,9 +43,10 @@ function NavigationManager({ products }) {
     containerRef.current && containerRef.current.navigate(name, params);
   };
 
-  const handleLogin = (loginData) => {
-    navigate('Products')
-    console.log('loginData: ', loginData); //Should be uploaded to AsyncStorage
+  const handleLogin = ({ username, password }) => { //Should be uploaded to AsyncStorage
+    if (username === 'A' && password === '1') {
+      navigate('Products')
+    }
   }
 
   return (
